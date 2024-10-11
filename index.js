cd2503fffee26ff73bda005bef2352bc5ce9bfc9
Caryolite <carl47063@gmail.com>
2024-10-11 23:30:41 +0800

    refactor: 把舊的功能整合一個EventListener

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function getRandomInt(min, max) {
 
 client.on(Events.MessageCreate,(message) => {
     if (message.author.bot) return;
+
     if (message.content === "hello") {
         message.channel.send("嗨");
     } else if (message.content === "戳") {
@@ -37,27 +38,21 @@ client.on(Events.MessageCreate,(message) => {
     } else if (message.content === "嗚哇><") {
         message.channel.send("？我沒戳你");
     } 
-})
 
-client.on(Events.MessageCreate,(message) => {
+    // 克里在嗎
     if (message.content === "克里在嗎") {
         let p = getRandomInt(1,2);
-        // console.log(p);
         if (p == 1){
             message.channel.send(`不在`);
         } else {
             message.channel.send(`我在喔:3`);
         }
     }
-})
 
 // 隨機
-client.on(Events.MessageCreate,(message) => {
-    if (message.author.bot) return;
     const reRandom = /隨機/.test(message);
     const reDie = /死/.test(message);
-    if (reRandom !== true) return;
-
+    if (reRandom == true) {
     let arr = message.content.split(" ");
     if ((arr[0]) !== "隨機") return;
 
@@ -71,14 +66,11 @@ client.on(Events.MessageCreate,(message) => {
             message.channel.send(`${arr[p]}`);
         }
     }
-})
+    }
 
 // 因數
-client.on(Events.MessageCreate,(message) => {
-    if (message.author.bot) return;
     const reFactor = /因數/.test(message);
-    if (reFactor == false) return;
-    
+    if (reFactor == true) {
     let num = message.content.split(" ");
     if ((num[0]) !== "因數") return;
 
@@ -98,10 +90,8 @@ client.on(Events.MessageCreate,(message) => {
     } else{
         message.channel.send(`${num[1]} 的因數有 [${factor} ]`);
     }
-})
-
-client.on(Events.MessageCreate,(message) => {
-    if (message.author.bot) return;
+    }
+    // 單抽
     if (message.content === "單抽"|message.content === "抽卡") {
         let gachaResult = getRandomInt(1, 100)
         if (gachaResult > 98){
@@ -112,12 +102,9 @@ client.on(Events.MessageCreate,(message) => {
             message.reply(`SR`);
         }
     };
-})
 
-client.on(Events.MessageCreate,(message) => {
+    // 十連
     if (message.content === "十連") {
-    if (message.author.bot) return;
-
     let gachaArr = [];
     let Guarantee = 0;
     for (let i = 1; i <= 10; i++){
@@ -136,7 +123,8 @@ client.on(Events.MessageCreate,(message) => {
         gachaArr[9] = ` SR`;
     }
     message.reply(`${gachaArr} `);
-}})
+    }
+})
 
 /* ------------------------------------------------------------------------- */
 
